c42ef063e8aad10ea738361e0d7cb5515ce2df78
gabriel-rado-movile <gabriel.rado@wavy.global>
2020-05-27 22:33:38 -0300

    gradle and gitignore

---
+++ build.gradle.kts
@@ -0,0 +1,29 @@
+plugins {
+    kotlin("jvm") version "1.3.72" // targeting jvm
+    id("org.springframework.boot") version "2.3.0.RELEASE" // adds spring boot
+    // use maven bom to version dependencies (set by boot)
+    id("io.spring.dependency-management") version "1.0.9.RELEASE"
+    // kotlin("plugin.spring") == id("org.jetbrains.kotlin.plugin.spring")
+    kotlin("plugin.spring") version "1.3.72" // open kotlin classes for spring
+}
+
+group = "com"
+version = "1"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-web") // spring web
+    implementation(kotlin("stdlib-jdk8")) // kotlin std lib for java 8
+}
+
+tasks {
+    compileKotlin {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+    compileTestKotlin {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+}
